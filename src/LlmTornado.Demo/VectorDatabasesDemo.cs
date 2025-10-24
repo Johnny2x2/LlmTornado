8078050b5b5f927d43ba7ed8ef8f27a883e7da38
jlomba <johnlomba1993@gmail.com>
2025-10-24 17:08:02 -0400

    updating the demo

--- src/LlmTornado.Demo/VectorDatabasesDemo.cs
+++ src/LlmTornado.Demo/VectorDatabasesDemo.cs
@@ -2,6 +2,7 @@
 using LlmTornado.Embedding.Models;
 using LlmTornado.VectorDatabases;
 using LlmTornado.VectorDatabases.Intergrations;
+using LlmTornado.VectorDatabases.Milvus.Integrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace LlmTornado.Demo;
 public class VectorDatabasesDemo
 {
     [TornadoTest]
+    [Flaky("Requires Chroma server running")]
     public static async Task TestChromaDB()
     {
         string query = "Function to add two numbers in python";
@@ -113,6 +115,54 @@ public class VectorDatabasesDemo
         VectorDocument vectorDocument2 = (VectorDocument)doc;
         Console.WriteLine($"VectorDocument2 Id: {vectorDocument2.Id}, Content: {vectorDocument2.Content}, Author: {vectorDocument2.Metadata["Author"]}");
 
+    }
+
+
+    [TornadoTest]
+    [Flaky("Requires Milvus server running")]
+    public static async Task TestMilvus()
+    {
+        string query = "Function to add two numbers in python";
+        string embeddingDescription = "A function that adds two numbers together in python";
+
+        Dictionary<string, object> metaData = new Dictionary<string, object>();
+        metaData.Add("FunctionName", "Function 1");
+
+        // Create a Milvus vector database connection
+        MilvusVectorDatabase vectorDb = new MilvusVectorDatabase(
+            host: "localhost",
+            port: 19530,
+            vectorDimension: 1536
+        );
+
+        await vectorDb.InitializeCollection("functions");
+
+        //Embed the function description and add to DB
+        TornadoApi tornadoApi = Program.Connect();
+        List<Task> tasks = new List<Task>();
+
+        EmbeddingResult? embInputResult;
+        EmbeddingResult? embQueryResult;
+        float[]? dataInput = [];
+        float[]? dataQuery = [];
+
+        tasks.Add(Task.Run(async () => { embInputResult = await tornadoApi.Embeddings.CreateEmbedding(EmbeddingModel.OpenAi.Gen3.Small, embeddingDescription); dataInput = embInputResult?.Data.FirstOrDefault()?.Embedding; }));
+        tasks.Add(Task.Run(async () => { embQueryResult = await tornadoApi.Embeddings.CreateEmbedding(EmbeddingModel.OpenAi.Gen3.Small, query); dataQuery = embQueryResult?.Data.FirstOrDefault()?.Embedding; }));
+
+        await Task.WhenAll(tasks);
+
+        //Add document to DB
+        VectorDocument vectorDocument = new VectorDocument(Guid.NewGuid().ToString(), embeddingDescription, metaData, dataInput);
+
+        await vectorDb.AddDocumentsAsync([vectorDocument]);
+
+        //Query DB for relevant functions
+        var queryData = await vectorDb.QueryByEmbeddingAsync(dataQuery, topK: 5);
+
+        foreach (var item in queryData)
+        {
+            Console.WriteLine($"Function Name: {item.Metadata?["FunctionName"]} \n Description: {item.Content}\n\n");
+        }
     }
 
 }
